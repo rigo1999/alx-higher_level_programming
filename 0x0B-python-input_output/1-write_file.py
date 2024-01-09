857eecb0d5407c17d4ec2d40b6aaed3e287ad4cf
root <rigo1999>
2024-01-09 14:12:29 -0800

    Write to a file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+ contains functiom
+that returns the number of lines
+of a text file
+"""
+
+
+def number_of_lines(filename=""):
+    """
+    returns number of lines
+    """
+    n = 0
+    with open(filename, encoding="utf-8") as my_file:
+        for line in my_file:
+            n += 1
+    return (n)
