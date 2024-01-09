fda468792abb21686e0a2dfcbca65f9e9122c198
root <rigo1999>
2024-01-09 14:21:17 -0800

    Write to a file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
- contains functiom
+This file contains functiom
 that returns the number of lines
 of a text file
 """
@@ -8,10 +8,10 @@ of a text file
 
 def number_of_lines(filename=""):
     """
-    returns number of lines
+    function returns number of lines
     """
     i = 0
-    with open(filename, encoding="utf-8") as my_file:
-        for line in my_file:
+    with open(filename, encoding="utf-8") as myFile:
+        for line in myFile:
             i += 1
     return (i)
