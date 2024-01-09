fb0a895aab32e6a8f3753cdfa1af69f0846024cf
root <rigo1999>
2024-01-09 14:17:42 -0800

    Write to a file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -10,8 +10,8 @@ def number_of_lines(filename=""):
     """
     returns number of lines
     """
-    n = 0
+    i = 0
     with open(filename, encoding="utf-8") as my_file:
         for line in my_file:
-            n += 1
-    return (n)
+            i += 1
+    return (i)
