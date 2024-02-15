3ed3f2efd7493c14d3d743af541fe5e40be7884a
root <rigo1999>
2024-02-15 12:49:23 -0800

    creatig database

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,3 +1,3 @@
 -- Write a script that creates a table called first_table in the current db
-mysql -u root -p'your_password' -e 'CREATE TABLE IF NOT EXISTS first_table(`id` INT, `name` VARCHAR(256));'
+CREATE table IF NOT EXISTS first_table(`id` INT, `name` VARCHAR(256));
 
