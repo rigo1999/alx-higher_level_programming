cea14eade92b885dda7e914ae2df72d38649356b
root <rigo1999>
2024-03-13 15:31:19 -0700

    Complete JS

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,4 @@
-#!/usr/bin/node 
-const lines = ["C is fun", "Python is cool", "JavaScript is amazing"];
-
-for (let i = 0; i < lines.length; i++) {
-  console.log(lines[i]);
-}
-
+#!/usr/bin/node
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
