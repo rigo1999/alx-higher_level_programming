712a10f7dde0e9e4ba9e1a0ec7fb66723a33da59
root <rigo1999>
2024-03-16 16:11:39 -0700

    sql injections

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""  lists all  the states from the database hbtn_0e_0_usa """
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    crt = db.crtsor()
+    match = sys.argv[4]
+    crt.execute("SELECT * FROM states WHERE name LIKE %s", (match, ))
+    rows = crt.fetchall()
+    for row in rows:
+        print(row)
+    crt.close()
+    db.close()
